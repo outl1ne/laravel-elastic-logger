323d077eb1dc31aa4c39fb1d4dd6b9fe6d0d5f03
Richard Raadi <richard@optimistdigital.com>
2023-01-03 15:45:58 +0200

    Get datetime in UTC time as Elastic does not support timezone formats

--- src/Listeners/PrepareLogPayload.php
+++ src/Listeners/PrepareLogPayload.php
@@ -3,12 +3,14 @@
 namespace Outl1ne\LaravelElasticLogger\Listeners;
 
 use DateTime;
+use DateTimeZone;
+use Exception;
 use Outl1ne\LaravelElasticLogger\Jobs\HandleSendLogToElastic;
 use Illuminate\Log\Events\MessageLogged;
 
 class PrepareLogPayload
 {
-    /** @var string  */
+    /** @var string */
     private string $excludeLogLevels;
 
     /**
@@ -26,35 +28,41 @@ class PrepareLogPayload
      *
      * @param MessageLogged $event
      * @return void
+     *
+     * @throws Exception
      */
     public function handle(MessageLogged $event): void
     {
-        if(in_array($event->level, explode(',', $this->excludeLogLevels))){
-            return;
+        try {
+            if (in_array($event->level, explode(',', $this->excludeLogLevels))) {
+                return;
+            }
+            $dateTime = new DateTime('NOW', new DateTimeZone('UTC'));
+            if (array_key_exists('exception', $event->context)) {
+                $exception = $event->context['exception'];
+                $logData = [
+                    'message' => $event->message,
+                    'level' => $event->level,
+                    'datetime' => $dateTime,
+                    'context' => [
+                        'exception_type' => get_class($exception),
+                        'exception_message' => $exception->getMessage(),
+                        'exception_file' => $exception->getFile(),
+                        'exception_line' => $exception->getLine()
+                    ]
+                ];
+            } else {
+                $logData = [
+                    'message' => $event->message,
+                    'level' => $event->level,
+                    'datetime' => $dateTime,
+                    'context' => $event->context
+                ];
+            }
+            HandleSendLogToElastic::dispatch($logData);
+        } catch (Exception $exception) {
+            // TODO - exception handling.
         }
-        $dateTime = new DateTime();
-        if (array_key_exists('exception', $event->context)) {
-            $exception = $event->context['exception'];
-            $logData = [
-                'message' => $event->message,
-                'level' => $event->level,
-                'datetime' => $dateTime,
-                'context' => [
-                    'exception_type' => get_class($exception),
-                    'exception_message' => $exception->getMessage(),
-                    'exception_file' => $exception->getFile(),
-                    'exception_line' => $exception->getLine()
-                ]
-            ];
-        } else {
-            $logData = [
-                'message' => $event->message,
-                'level' => $event->level,
-                'datetime' => $dateTime,
-                'context' => $event->context
-            ];
-        }
-        HandleSendLogToElastic::dispatch($logData);
     }
 }
 
